581e88925b1b2ae897ca8f42ab3c28d47026fb5e
Dxftoro <96373847+Dxftoro@users.noreply.github.com>
2024-10-12 10:42:40 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,21 +2,47 @@
 #include "stdlib.h"
 
 #define CMD_BUFFSIZE 1024
+#define true 1
+#define false 0
 
 enum ERR {
 	BUFF_MEM_ALLOC_ERROR,
 	BUFF_ERROR
 };
 
+void AllocCheck(char* buffer) {
+	if (!buffer) {
+		fprintf(stderr, "BUFF_MEM_ALLOC_ERROR");
+		exit(1);
+	}
+}
+
 char* ReadLine(void) {
 	int buffSize = CMD_BUFFSIZE;
 	int position = 0;
 	char* buffer = malloc(sizeof(char) * buffSize);
-	int c;
+	int sym;
 
-	if (!buffer) {
-		fprintf(stderr, "BUFF_MEM_ALLOC_ERROR");
-		exit(1);
+	AllocCheck(buffer);
+
+	while (true) {
+		sym = getchar();
+
+		if (sym == EOF || sym == '\0') {
+			buffer[position] = '\0';
+			return buffer;
+		}
+		else buffer[position] = sym;
+
+		if (position >= buffSize) {
+			buffSize += CMD_BUFFSIZE;
+
+			buffer = realloc(buffer, buffSize);
+
+			AllocCheck(buffer);
+		}
+
+		position++;
 	}
 }
 
@@ -25,9 +51,14 @@ void Loop(void) {
 	char** args;
 	int status = 0;
 
-	do { 
+	while (status) {
 		printf("> ");
-	} while(status);
+		
+		ReadLine();
+
+		free(line);
+		free(args);
+	}
 }
 
 int main(int argc, char **argv) {
